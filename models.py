8d37fc1f88e85a12f3b37269587e6cfde978acf2
WeaverC20 <67601091+WeaverC20@users.noreply.github.com>
2024-04-29 16:13:34 -0400

    finished merging transient and uncertainty

--- models.py
+++ models.py
@@ -369,6 +369,188 @@ def t_transport_sim(
     return k, average_conc, total_flux
 
 
+# def transient_t_transport_sim(
+#     temperature_field,
+#     mesh_fenics,
+#     velocity,
+#     volume_markers,
+#     surface_markers,
+#     correspondance_dict,
+#     D_0=1.508521565198744e-08,  # TODO take this from HTM
+#     E_D=0.23690444592353738,  # TODO take this from HTM
+# ):
+#     """
+#     Takes in a list of temperatures and a set mesh and returns a list of diffusion coefficients that correspond to each temperature
+
+#     Args:
+#         temperature_field (fenics.Function): the temperature field in K
+#         mesh_fenics (fenics.Mesh): the mesh (should be the same as the one used to compute the temperature field)
+#         velocity (fenics.Function): the velocity field in m/s
+
+#     Returns:
+#         float: the mass transport coefficient in m/s
+#     """
+#     # setting up current simulation
+#     model_2d = F.Simulation()
+
+#     # D, E_d source: "nakamura_hydrogen_2015"
+#     flibe_mat = F.Material(
+#         id=1,
+#         D_0=D_0,
+#         E_D=E_D,
+#     )
+#     model_2d.materials = F.Materials([flibe_mat])
+
+#     # creating mesh with festim
+#     model_2d.mesh = F.Mesh(
+#         mesh=mesh_fenics,  # TODO we should be able to get the mesh from the temperature field
+#         volume_markers=volume_markers,
+#         surface_markers=surface_markers,
+#     )
+
+#     temperature_file = "temperature.xdmf"
+#     f.XDMFFile(temperature_file).write_checkpoint(
+#         temperature_field,
+#         "temperature",
+#         0,
+#         f.XDMFFile.Encoding.HDF5,
+#         append=False,
+#     )
+#     model_2d.T = F.TemperatureFromXDMF(temperature_file, label="temperature")
+
+#     # setting up T source
+#     rthetaz = f.SpatialCoordinate(mesh_fenics)
+#     salt_volume = 2 * f.pi * f.assemble(rthetaz[0] * f.dx())
+#     print(f"Salt volume : {salt_volume} m3, {salt_volume * 1e6} mL")
+
+#     measured_tritium_source = 3.65e5  # T/s
+
+#     twelve_hr = 12 * 3600
+
+#     func = Piecewise(
+#         (measured_tritium_source, (F.t >= 0) & (F.t <= twelve_hr)),
+#         (0, (F.t > twelve_hr) & (F.t < 2 * twelve_hr)),
+#         (measured_tritium_source, (F.t >= 2 * twelve_hr) & (F.t <= 3 * twelve_hr)),
+#         (0, True),
+#     )
+
+#     model_2d.sources = [F.Source(value=func / salt_volume, volume=1, field=0)]
+
+#     top_id = correspondance_dict["top"]
+#     bottom_id = correspondance_dict["bottom"]
+#     right_id = correspondance_dict["right"]
+#     left_id = correspondance_dict["left"]
+#     upper_left_id = correspondance_dict["upper_left"]
+#     left_top_id = correspondance_dict["left_top"]
+
+#     # setting up transport boundary conditions
+#     tritium_transport_bcs = [
+#         F.DirichletBC(
+#             surfaces=[top_id, bottom_id, right_id, left_top_id, upper_left_id],
+#             value=0,
+#             field=0,
+#         )
+#     ]
+
+#     model_2d.boundary_conditions = tritium_transport_bcs
+
+#     model_2d.dt = F.Stepsize(
+#         initial_value=100,
+#         stepsize_change_ratio=1.05,
+#         milestones=[twelve_hr, 2 * twelve_hr, 3 * twelve_hr],
+#     )
+
+#     # simulation parameters and running model
+#     model_2d.settings = F.Settings(
+#         absolute_tolerance=1e-10, relative_tolerance=1e-10, final_time=14 * twelve_hr
+#     )
+
+#     # setting up exports
+#     export_folder = "Results_Transient"
+
+#     top_flux = SurfaceFluxCylindrical(field="solute", surface=top_id)
+#     right_flux = SurfaceFluxCylindrical(field="solute", surface=right_id)
+#     bottom_flux = SurfaceFluxCylindrical(field="solute", surface=bottom_id)
+#     upper_left_flux = SurfaceFluxCylindrical(field="solute", surface=upper_left_id)
+#     left_flux = SurfaceFluxCylindrical(field="solute", surface=left_id)
+#     left_top_flux = SurfaceFluxCylindrical(field="solute", surface=left_top_id)
+#     total_volume = TotalVolumeCylindrical(field="solute", volume=1)
+
+#     derived_quantities = F.DerivedQuantities(
+#         [
+#             top_flux,
+#             right_flux,
+#             bottom_flux,
+#             upper_left_flux,
+#             left_flux,
+#             left_top_flux,
+#             AverageVolumeCylindrical(field="solute", volume=1),
+#             total_volume,
+#         ],
+#         filename=export_folder + "/simulation.csv",
+#     )
+
+#     model_2d.exports = F.Exports(
+#         [
+#             F.XDMFExport("solute", folder=export_folder),
+#             F.XDMFExport("retention", folder=export_folder),
+#             F.XDMFExport("T", folder=export_folder),
+#             derived_quantities,
+#         ]
+#     )
+#     # adding advection
+#     model_2d.initialise()  # reinitialisation is needed
+
+#     hydrogen_concentration = model_2d.h_transport_problem.mobile.solution
+#     test_function_solute = model_2d.h_transport_problem.mobile.test_function
+
+#     advection_term = (
+#         f.inner(f.dot(f.grad(hydrogen_concentration), velocity), test_function_solute)
+#         * model_2d.mesh.dx
+#     )
+
+#     model_2d.h_transport_problem.F += advection_term
+
+#     # model_2d.log_level = 20
+
+#     model_2d.run()
+
+#     plt.figure()
+#     plt.title("Hydrogen concentration")
+#     CS = f.plot(hydrogen_concentration)
+#     plt.colorbar(CS, label="H/m3")
+#     plt.axis("off")
+#     plt.show()
+
+#     # reading results
+#     my_data = np.genfromtxt(
+#         export_folder + "/simulation.csv", names=True, delimiter=","
+#     )
+
+#     flux_1 = my_data["Flux_surface_1_solute"]
+#     flux_2 = my_data["Flux_surface_2_solute"]
+#     flux_3 = my_data["Flux_surface_3_solute"]
+#     flux_4 = my_data["Flux_surface_4_solute"]
+#     flux_5 = my_data["Flux_surface_5_solute"]
+#     flux_6 = my_data["Flux_surface_6_solute"]
+
+#     # calculating diffusion coefficient
+#     total_flux = abs(flux_1 + flux_2 + flux_3 + flux_4 + flux_5 + flux_6)
+#     top_flux = abs(flux_2)
+
+#     average_conc = my_data["Average_solute_volume_1"]
+
+#     total_surface = 2 * np.pi * f.assemble(rthetaz[0] * model_2d.mesh.ds)
+#     print(f"Total surface: {total_surface:.2e} m2")
+#     k = total_flux / (total_surface * average_conc)
+#     k_top = top_flux / (2 * np.pi * f.assemble(rthetaz[0] * model_2d.mesh.ds))
+
+#     print(f"Total flux: {total_flux:.2e} H/s/m")
+#     print(f"Average concentration: {average_conc:.2e} H/m3")
+#     print(f"k: {k:.2e} m/s")
+
+#     return k_top, average_conc, total_flux
+
 def transient_t_transport_sim(
     temperature_field,
     mesh_fenics,
@@ -457,7 +639,7 @@ def transient_t_transport_sim(
     model_2d.dt = F.Stepsize(
         initial_value=100,
         stepsize_change_ratio=1.05,
-        milestones=[twelve_hr, 2 * twelve_hr, 3 * twelve_hr],
+        # milestones=[twelve_hr, 2 * twelve_hr, 3 * twelve_hr],
     )
 
     # simulation parameters and running model
@@ -522,31 +704,24 @@ def transient_t_transport_sim(
     plt.axis("off")
     plt.show()
 
-    # reading results
-    my_data = np.genfromtxt(
-        export_folder + "/simulation.csv", names=True, delimiter=","
+    top_flux = np.abs(top_flux.data)
+    wall_flux = np.abs(
+        np.array(right_flux.data)
+        + np.array(left_flux.data)
+        + np.array(upper_left_flux.data)
+        + np.array(bottom_flux.data)
+        + np.array(left_top_flux.data)
     )
 
-    flux_1 = my_data["Flux_surface_1_solute"]
-    flux_2 = my_data["Flux_surface_2_solute"]
-    flux_3 = my_data["Flux_surface_3_solute"]
-    flux_4 = my_data["Flux_surface_4_solute"]
-    flux_5 = my_data["Flux_surface_5_solute"]
-    flux_6 = my_data["Flux_surface_6_solute"]
+    # average_conc = my_data["Average_solute_volume_1"]
+    # total_vol = my_data["Total_solute_volume_1"]
 
-    # calculating diffusion coefficient
-    total_flux = abs(flux_1 + flux_2 + flux_3 + flux_4 + flux_5 + flux_6)
-    top_flux = abs(flux_2)
+    # total_surface = 2 * np.pi * f.assemble(rthetaz[0] * model_2d.mesh.ds)
+    # print(f"Total surface: {total_surface:.2e} m2")
+    # k = total_flux / (total_surface * average_conc)
 
-    average_conc = my_data["Average_solute_volume_1"]
+    # print(f"Total flux: {total_flux:.2e} H/s/m")
+    # print(f"Average concentration: {average_conc:.2e} H/m3")
+    # print(f"k: {k:.2e} m/s")
 
-    total_surface = 2 * np.pi * f.assemble(rthetaz[0] * model_2d.mesh.ds)
-    print(f"Total surface: {total_surface:.2e} m2")
-    k = total_flux / (total_surface * average_conc)
-    k_top = top_flux / (2 * np.pi * f.assemble(rthetaz[0] * model_2d.mesh.ds))
-
-    print(f"Total flux: {total_flux:.2e} H/s/m")
-    print(f"Average concentration: {average_conc:.2e} H/m3")
-    print(f"k: {k:.2e} m/s")
-
-    return k_top, average_conc, total_flux
+    return wall_flux, top_flux, np.array(total_volume.data), derived_quantities.t
